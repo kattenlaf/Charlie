7c3a4a10afed257ff5215f43a06be5e4ece07b4b
kattenlaf <ruelgordon123@gmail.com>
2025-08-22 00:36:12 -0700

    added some routing for front end

--- Frontend/chibi_usa/app/_layout.tsx
+++ Frontend/chibi_usa/app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="send_image_screen" options={{ title: 'Send Image' }} />
+    </Stack>
+  );
 }
--- Frontend/chibi_usa/app/index.tsx
+++ Frontend/chibi_usa/app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+
+const Home = () => {
+  const navigation = useNavigation();
+
+  const SendImageScreenButton = () => {
+  return(
+    <Pressable style={styles.button} onPress={() => navigation.navigate('send_image_screen' as never)}>
+      <Text style={styles.buttonText}>Go to Send Image Screen</Text>
+    </Pressable>
+    );
+  };
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: true, title: "Home" });
+  }, [navigation]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Login screen here or something</Text>
+      <SendImageScreenButton />
     </View>
   );
 }
+
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#1ef44cff",
+    padding: 5,
+    borderRadius: 10,
+    alignContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+    padding: 10,
+  }
+});
